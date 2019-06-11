981f8c5c87c74b4fe8b71b0a32ad5f10ef903f99
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-11 22:45:30 +0100

    Remove boilerplate, add deps and title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PollenCount />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import PollenCount from './components/PollenCount.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PollenCount
   }
 }
 </script>
